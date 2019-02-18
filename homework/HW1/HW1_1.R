558e60b995330e103b6812523e6607f1a999a62e
Bhaven Patel <3346052+bhavenp@users.noreply.github.com>
2019-02-18 18:17:54 -0500

    made some attempt at problem 1

--- homework/HW1/HW1_1.R
+++ homework/HW1/HW1_1.R
@@ -10,4 +10,24 @@
 data <- read.csv("../../data/FultonPUMS5full.csv");
 
 puma_regions <- unique(data$puma);
+puma_regions_table <- table(data$puma);
+puma_regions_table
 
+# puma_1101 <- data[data$puma == 1101, ];
+dups <- data[duplicated(data), ]
+
+
+ages = c();
+education = c();
+num_unique = c();
+for(a in 18:100){
+  for(e in 1:16){
+    query <- data[data$puma == 1101 & data$age==a & data$educ==e, ];
+    num_unique <- c( num_unique, nrow(query));
+    ages <- c(ages, a);
+    education <- c(education, e);
+  }
+}
+query_df <- data.frame("Age" = ages, "Education" = education, "Unique" = num_unique);
+
+unq_query_df <- query_df[query_df$Unique == 1, ]
