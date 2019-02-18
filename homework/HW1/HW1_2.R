76ab2949afd19bdfddc44070190805687022b9e2
Bhaven Patel <3346052+bhavenp@users.noreply.github.com>
2019-02-18 18:18:14 -0500

    started on problem 2

---
+++ homework/HW1/HW1_2.R
@@ -0,0 +1,10 @@
+##
+##  HW1_2.r
+##
+##  Implement a reconstruction attack on the sample of 100 points from the PUMS dataset
+##
+##  BP 2.18.2019
+##
+
+#read in the sample of 100 individuals
+pums_100 <- read.csv(file="../../data/FultonPUMS5sample100.csv")
